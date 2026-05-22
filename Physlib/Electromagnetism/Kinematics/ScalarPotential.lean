34c8ed16d334b840d141190c6f5967a8bd4f1a67
Joseph Tooby-Smith <72603918+jstoobysmith@users.noreply.github.com>
2026-05-22 10:01:28 +0100

    rm: scalarPotential

--- Physlib/Electromagnetism/Kinematics/ScalarPotential.lean
+++ Physlib/Electromagnetism/Kinematics/ScalarPotential.lean
@@ -201,36 +201,4 @@ lemma scalarPotential_differentiable_time {d} (c : SpeedOfLight) (A : Electromag
 
 end ElectromagneticPotential
 
-/-!
-
-## E. Scalar potential for distributions
-
--/
-
-namespace DistElectromagneticPotential
-open TensorSpecies
-open Tensor
-open SpaceTime
-open TensorProduct
-open minkowskiMatrix
-attribute [-simp] Fintype.sum_sum_type
-attribute [-simp] Nat.succ_eq_add_one
-
-set_option backward.isDefEq.respectTransparency false in
-/-- The scalar potential of an electromagnetic potential which is a distribution. -/
-noncomputable def scalarPotential {d} (c : SpeedOfLight) :
-    DistElectromagneticPotential d →ₗ[ℝ]
-    (Time × Space d) →d[ℝ] ℝ where
-  toFun A := Lorentz.Vector.temporalCLM d ∘L distTimeSlice c (c.val • A)
-  map_add' A₁ A₂ := by
-    ext ε
-    simp [distTimeSlice]
-  map_smul' r A := by
-    ext ε
-    simp only [distTimeSlice, map_smul, ContinuousLinearEquiv.coe_mk, LinearEquiv.coe_mk,
-      LinearMap.coe_mk, AddHom.coe_mk, ContinuousLinearMap.coe_comp', ContinuousLinearMap.coe_smul',
-      Function.comp_apply, Pi.smul_apply, smul_eq_mul, Real.ringHom_apply]
-    ring
-
-end DistElectromagneticPotential
 end Electromagnetism
