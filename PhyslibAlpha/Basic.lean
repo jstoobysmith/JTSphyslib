48882bcdb0a15fd89de9758d8d7726dd2adec288
jstoobysmith <72603918+jstoobysmith@users.noreply.github.com>
2026-06-08 09:41:28 -0600

    feat: fix linters

--- PhyslibAlpha/Basic.lean
+++ PhyslibAlpha/Basic.lean
@@ -1,9 +1,9 @@
-module
 /-
 Copyright (c) 2026 Joseph Tooby-Smith. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Joseph Tooby-Smith
 -/
+module
 public import Physlib.Meta.TODO.Basic
 /-!
 
