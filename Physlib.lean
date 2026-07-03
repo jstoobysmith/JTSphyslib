75b77973e90f0dc604419e11c4dde3b366d345d5
jstoobysmith <72603918+jstoobysmith@users.noreply.github.com>
2026-07-03 06:57:11 +0100

    feat: Address comments

--- Physlib.lean
+++ Physlib.lean
@@ -163,7 +163,7 @@ public import Physlib.Particles.StandardModel.AnomalyCancellation.NoGrav.One.Lem
 public import Physlib.Particles.StandardModel.AnomalyCancellation.NoGrav.One.LinearParameterization
 public import Physlib.Particles.StandardModel.AnomalyCancellation.Permutations
 public import Physlib.Particles.StandardModel.Basic
-public import Physlib.Particles.StandardModel.Fermions.Quarks
+public import Physlib.Particles.StandardModel.Fermions.QuarkDoublet
 public import Physlib.Particles.StandardModel.HiggsBoson.Basic
 public import Physlib.Particles.StandardModel.HiggsBoson.EffectivePotential
 public import Physlib.Particles.StandardModel.HiggsBoson.Potential
